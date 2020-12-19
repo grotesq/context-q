86faab6bedb3a5c2e1075c80faa1490d84880199
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2020-12-19 18:59:52 +0900

    v1.1.7

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,12 +4,12 @@ interface ProviderProps {
     children: React.ReactNodeArray | React.ReactNode;
     defaultState: any;
 }
-export declare function Provider(props: ProviderProps): JSX.Element;
+export declare const Provider: (props: ProviderProps) => JSX.Element;
 export declare const withContext: (Component: any) => any;
 export declare function useContextQ(): any;
 declare const _default: {
     Context: React.Context<null>;
-    Provider: typeof Provider;
+    Provider: (props: ProviderProps) => JSX.Element;
     withContext: (Component: any) => any;
     useContextQ: typeof useContextQ;
 };
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -9,31 +10,55 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-import React, { createContext, useContext, useState } from 'react';
-export var Context = createContext(null);
-var ContextProvider = Context.Provider, Consumer = Context.Consumer;
-export function Provider(props) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useContextQ = exports.withContext = exports.Provider = exports.Context = void 0;
+var react_1 = __importStar(require("react"));
+exports.Context = react_1.createContext(null);
+var ContextProvider = exports.Context.Provider, Consumer = exports.Context.Consumer;
+var Provider = function (props) {
     function update(state) {
         setState(__assign(__assign({}, state), { update: update }));
     }
-    var _a = useState(__assign(__assign({}, props.defaultState), { update: update })), state = _a[0], setState = _a[1];
-    return (React.createElement(ContextProvider, { value: state }, props.children));
-}
-export var withContext = function (Component) {
-    var WithContextComponent = function (props) { return (React.createElement(Consumer, null, function (context) { return React.createElement(Component, __assign({}, props, { context: context })); })); };
+    var _a = react_1.useState(__assign(__assign({}, props.defaultState), { update: update })), state = _a[0], setState = _a[1];
+    return react_1.default.createElement(ContextProvider, { value: state }, props.children);
+};
+exports.Provider = Provider;
+var withContext = function (Component) {
+    var WithContextComponent = function (props) { return (react_1.default.createElement(Consumer, null, function (context) { return react_1.default.createElement(Component, __assign({}, props, { context: context })); })); };
     // for Next.js
     if (Component.getInitialProps) {
         WithContextComponent.getInitialProps = Component.getInitialProps;
     }
     return WithContextComponent;
 };
-export function useContextQ() {
-    return useContext(Context);
+exports.withContext = withContext;
+function useContextQ() {
+    return react_1.useContext(exports.Context);
 }
-export default {
-    Context: Context,
-    Provider: Provider,
-    withContext: withContext,
+exports.useContextQ = useContextQ;
+exports.default = {
+    Context: exports.Context,
+    Provider: exports.Provider,
+    withContext: exports.withContext,
     useContextQ: useContextQ,
 };
 //# sourceMappingURL=index.js.map
